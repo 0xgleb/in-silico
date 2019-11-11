7ab6ba5540b7e60d14e467b7a8dce84c2ac8abd4
Gleb Dianov <gleb.dianov@habito.com>
2019-11-11 17:36:05 +0000

    implement DNA complement

--- src/DNA.hs
+++ src/DNA.hs
@@ -1,9 +1,84 @@
 module DNA
-  (
+  ( DNANucs
+  -- , DNANuc
+  , DNANucleotide
+  , pattern A
+  , pattern C
+  , pattern T
+  , pattern G
+
+  , parseDNANucs
+
+  , complement
+  , reverseComplement
   )
   where
 
 import qualified Nucleotide as Nuc
 
-type DNANuc
+import qualified Prelude
+import           Protolude       hiding (complement)
+import           Test.QuickCheck
+
+type DNANucs
   = '[Nuc.A, Nuc.C, Nuc.T, Nuc.G]
+
+-- class (nuc `Elem` DNANucs ~ 'True) => DNANuc (nuc :: Nucleotide) where
+
+-- instance DNANuc 'A where
+-- instance DNANuc 'C where
+-- instance DNANuc 'T where
+-- instance DNANuc 'G where
+
+newtype DNANucleotide
+  = DNANucleotide { getDNANucleotide :: Nuc.Nucleotide }
+  deriving newtype (Show, Eq)
+  deriving newtype (Arbitrary)
+
+pattern A :: DNANucleotide
+pattern A = DNANucleotide Nuc.A
+
+pattern C :: DNANucleotide
+pattern C = DNANucleotide Nuc.C
+
+pattern T :: DNANucleotide
+pattern T = DNANucleotide Nuc.T
+
+pattern G :: DNANucleotide
+pattern G = DNANucleotide Nuc.G
+
+{-# COMPLETE A, C, T, G #-}
+
+mkDNANucleotide :: Nuc.Nucleotide -> Maybe DNANucleotide
+mkDNANucleotide = \case
+  Nuc.A -> Just A
+  Nuc.C -> Just C
+  Nuc.T -> Just T
+  Nuc.G -> Just G
+  _     -> Nothing
+
+
+parseDNANucs :: Prelude.String -> Maybe [DNANucleotide]
+parseDNANucs
+  = sequence . fmap mkDNANucleotide <=< Nuc.parseNucs
+
+
+complement :: [DNANucleotide] -> [DNANucleotide]
+complement = fmap $ \case
+  A -> T
+  C -> G
+  T -> A
+  G -> C
+
+reverseComplement :: [DNANucleotide] -> [DNANucleotide]
+reverseComplement = reverse . complement
+
+-- complement :: NucList DNANuc -> NucList DNANuc
+-- complement = nucMap $ \case
+--   TSin -> ASin
+--   ASin -> TSin
+--   CSin -> GSin
+--   GSin -> CSin
+
+-- reverseComplement :: NucList DNANuc -> NucList DNANuc
+-- reverseComplement = reverseNucList . complement
--- src/Nucleotide.hs
+++ src/Nucleotide.hs
@@ -1,29 +1,87 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
+
 module Nucleotide
   ( Nucleotide(..)
+  , NucleotideSin(..)
+
+  , NucList(..)
+  -- , nucMap
+  , reverseNucList
+
+  , Elem
+
   , parseNucs
   , countNucs
   )
   where
 
-import qualified Data.Map.Lazy as Map
+import qualified Data.Map.Lazy   as Map
 import qualified Prelude
 import           Protolude
 import qualified Safe
+import           Test.QuickCheck
 
 data Nucleotide
   = A
   | C
   | T
   | G
-  deriving (Show, Read, Eq, Ord)
+  | U
+  deriving stock (Show, Read, Eq, Ord, Enum)
+
+instance Arbitrary Nucleotide where
+  arbitrary = toEnum . flip mod 4 . abs <$> arbitrary
 
 data NucleotideSin (a :: Nucleotide) where
   ASin :: NucleotideSin A
   CSin :: NucleotideSin C
   TSin :: NucleotideSin T
   GSin :: NucleotideSin G
+  USin :: NucleotideSin U
 
-type family Elem (x :: Type) (xs :: [Type]) :: Bool where
+data NucList (c :: Nucleotide -> Constraint) where
+  NucNil  :: NucList c
+  NucCons :: c nuc => NucleotideSin nuc -> NucList c -> NucList c
+
+instance Semigroup (NucList c) where
+  NucNil <> list = list
+
+  (n `NucCons` ns) <> list
+    = n `NucCons` (ns <> list)
+
+type family Function (tag :: k) (input :: arg) :: arg
+
+-- nucMap
+--   :: forall tag cPrev cNext
+--    . ( forall nuc. (cPrev nuc, cNext (Function tag nuc))
+--      => NucleotideSin nuc -> NucleotideSin (Function tag nuc)
+--      )
+
+--   -> NucList cPrev
+--   -> NucList cNext
+
+-- nucMap func = \case
+--   NucNil ->
+--     NucNil
+
+--   NucCons n ns ->
+--     n `NucCons` nucMap func ns
+
+singletonNuc :: c nuc => NucleotideSin nuc -> NucList c
+singletonNuc = flip NucCons NucNil
+
+reverseNucList :: NucList c -> NucList c
+reverseNucList = reverseNucTail NucNil
+  where
+    reverseNucTail :: NucList c -> NucList c -> NucList c
+    reverseNucTail accum = \case
+      NucNil ->
+        accum
+
+      NucCons n ns ->
+        reverseNucTail (accum <> singletonNuc n) ns
+
+type family Elem (x :: k) (xs :: [k]) :: Bool where
   x `Elem` '[]       = 'False
   x `Elem` (x ': xs) = 'True
   x `Elem` (y ': xs) = x `Elem` xs
