d2392b301047966b0366e5b7e20da5c8b40cb657
Gleb Dianov <gleb.dianov@habito.com>
2020-01-31 14:50:17 +0000

    calculate RNA folds

--- src/RNA/Fold.hs
+++ src/RNA/Fold.hs
@@ -1,5 +1,12 @@
 module RNA.Fold
   ( Score(..)
+
+  , Fold(..)
+  , showFold
+  , flatPair
+
+  , foldAndShow
+  , finalScore
   , score
   , scoreWatsonCrick
   )
@@ -9,12 +16,45 @@ import GoldenStandard
 import RNA.Molecule
 
 import qualified Data.Map.Lazy as Map
+import qualified Prelude
+import qualified Debug.Trace as Debug
+
+data Fold
+  = Pair [Fold]
+  | UnpairedNuc
+  deriving (Show, Eq)
+
+showFold :: [Fold] -> Text
+showFold = foldMap showAFold
+
+  where
+    showAFold = \case
+      Pair innerFold ->
+        "(" <> showFold innerFold <> ")"
+
+      UnpairedNuc ->
+        "."
+
 
 newtype Score
   = Score { getScore :: Integer }
   deriving newtype (Show, Eq, Ord, Num)
 
-scoreWatsonCrick :: [Nucleotide] -> Map [Nucleotide] Score
+flatPair :: Fold
+flatPair = Pair []
+
+foldAndShow :: [Nucleotide] -> Text
+foldAndShow nucs
+  = let folds = snd $ finalScore nucs
+     in foldMap show nucs <> "\n" <> showFold folds
+
+finalScore :: [Nucleotide] -> (Score, [Fold])
+finalScore nucs
+  = fromMaybe (Prelude.error "No score for the full sequence!")
+  $ Map.lookup nucs
+  $ scoreWatsonCrick nucs
+
+scoreWatsonCrick :: [Nucleotide] -> Map [Nucleotide] (Score, [Fold])
 scoreWatsonCrick = score $ \x y ->
   if x == complement y then 1 else 0
 
@@ -22,17 +62,17 @@ scoreWatsonCrick = score $ \x y ->
 score
   :: (Nucleotide -> Nucleotide -> Score)
   -> [Nucleotide]
-  -> Map [Nucleotide] Score
+  -> Map [Nucleotide] (Score, [Fold])
 
 score scorePair rnaSeq
-  = combine 3 rnaSeq $ scorePairs rnaSeq mempty
+  = combine 2 rnaSeq initialScoreMap
 
   where
     combine
       :: Int
       -> [Nucleotide]
-      -> Map [Nucleotide] Score
-      -> Map [Nucleotide] Score
+      -> Map [Nucleotide] (Score, [Fold])
+      -> Map [Nucleotide] (Score, [Fold])
 
     combine n nucs scoreMap
       | n > length rnaSeq = scoreMap
@@ -40,57 +80,72 @@ score scorePair rnaSeq
       | length cur /= n = combine (n + 1) rnaSeq scoreMap
 
       | otherwise
-      = flip (Map.insert cur) scoreMap
-      $ foldl max 0
-          [ pairedScore
-          , unpairedScore $ drop 1 cur
-          , unpairedScore $ take (n - 1) cur
-          , bifurcation 0 1
-          ]
+      = let
+          folder pp@(_, prev) nn@(_, next)
+            = if fst prev <= fst next then nn else pp
+
+          (msg, res)
+            = foldl folder ("", (0, []))
+            [ ("paired", pairedScore)
+            , ("unpaired left",  unpairedScore True  $ drop 1 cur)
+            , ("unpaired right", unpairedScore False $ take (n - 1) cur)
+            , ("bifurcation", bifurcation (0, []) 1)
+            ]
+
+        in combine n (drop 1 nucs)
+             $ flip (Map.insert cur) scoreMap
+             $ debugTrace (msg <> ": " <> show @_ @Text cur) res
 
       where
         cur = take n nucs
 
+        debugTrace :: Show a => Text -> a -> a
+        debugTrace comment val
+          = if False -- show @_ @Text cur == "[A,U,G,G,C]"
+            then Debug.traceShow (comment <> ": " <> show @_ @Text val) val
+            else val
+
+        pairedScore :: (Score, [Fold])
         pairedScore
           = Map.lookup (drop 1 $ take (n - 1) cur) scoreMap
+
           & let pair = fromMaybe 0 $ scorePair <$> headMay cur <*> lastMay cur
-            in  maybe pair (+ pair)
 
-        unpairedScore key
-          = fromMaybe 0 $ Map.lookup key scoreMap
+            in  maybe (pair, [Pair []])
+                  $ \(innerScore, innerFold) -> (innerScore + pair, [Pair innerFold])
 
-        bifurcation :: Score -> Int -> Score
+        unpairedScore :: Bool -> [Nucleotide] -> (Score, [Fold])
+        unpairedScore isInTheBeginning key
+          = let (innerScore, innerFold) = fromMaybe (0, []) $ Map.lookup key scoreMap
+            in  if isInTheBeginning
+                   then (innerScore, UnpairedNuc : innerFold)
+                   else (innerScore, innerFold <> [UnpairedNuc])
+
+        bifurcation :: (Score, [Fold]) -> Int -> (Score, [Fold])
         bifurcation maxScore size
           | size == n = maxScore
 
-          | currentScore > maxScore
-          = bifurcation currentScore $ size + 1
+          | currentScore >= fst maxScore
+          = bifurcation current $ size + 1
 
           | otherwise
           = bifurcation maxScore $ size + 1
 
           where
-            firstPart  = take size cur
-            secondPart = drop size cur
+            (firstScore, firstFold)
+              = fromMaybe (0, []) (Map.lookup (take size cur) scoreMap)
 
-            currentScore
-              = fromMaybe 0 (Map.lookup firstPart  scoreMap)
-              + fromMaybe 0 (Map.lookup secondPart scoreMap)
+            (secondScore, secondFold)
+              = fromMaybe (0, []) (Map.lookup (drop size cur) scoreMap)
 
+            current@(currentScore, _)
+              = ( firstScore + secondScore
+                , debugTrace "newFold" $ debugTrace "firstFold" firstFold <> debugTrace "secondFold" secondFold
+                )
 
-    scorePairs
-      :: [Nucleotide]
-      -> Map [Nucleotide] Score
-      -> Map [Nucleotide] Score
-
-    scorePairs (x : y : rest) scoreMap
-      = scorePairs (y : rest) (newMap scoreMap)
-
-      where
-        newMap = Map.alter upd [x, y]
-
-        upd = Just . \case
-          Nothing  -> scorePair x y
-          Just val -> val
-
-    scorePairs _ scoreMap = scoreMap
+    initialScoreMap
+      = Map.insert [A] (0, [UnpairedNuc])
+      . Map.insert [U] (0, [UnpairedNuc])
+      . Map.insert [G] (0, [UnpairedNuc])
+      . Map.insert [C] (0, [UnpairedNuc])
+      $ mempty
--- test/RNA/FoldSpec.hs
+++ test/RNA/FoldSpec.hs
@@ -17,74 +17,121 @@ spec = do
     it "returns the number of hydrogen bonds in the optimal RNA fold" $ do
       let Right rna = parseRNASeq rnaSeq
 
-          expectedMap :: Map [Nucleotide] Score
+          expectedMap :: Map [Nucleotide] (Score, [Fold])
           expectedMap
-            = Map.insert [A, U] 1
-            . Map.insert [U, G] 0
-            . Map.insert [G, G] 0
-            . Map.insert [G, C] 1
-            . Map.insert [C, A] 0
-            . Map.insert [A, U] 1
-            . Map.insert [U, C] 0
-            . Map.insert [C, G] 1
-            . Map.insert [G, C] 1
+            = Map.insert [A] (0, [UnpairedNuc])
+            . Map.insert [U] (0, [UnpairedNuc])
+            . Map.insert [G] (0, [UnpairedNuc])
+            . Map.insert [C] (0, [UnpairedNuc])
 
-            . Map.insert [A, U, G] 1
-            . Map.insert [U, G, G] 0
-            . Map.insert [G, G, C] 1
-            . Map.insert [G, C, A] 1
-            . Map.insert [C, A, U] 1
-            . Map.insert [A, U, C] 1
-            . Map.insert [U, C, G] 1
-            . Map.insert [C, G, G] 1
-            . Map.insert [G, G, C] 1
+            . Map.insert [A, U] (1, [flatPair])
+            . Map.insert [U, G] (0, [UnpairedNuc, UnpairedNuc])
+            . Map.insert [G, G] (0, [UnpairedNuc, UnpairedNuc])
+            . Map.insert [G, C] (1, [flatPair])
+            . Map.insert [C, A] (0, [UnpairedNuc, UnpairedNuc])
+            . Map.insert [A, U] (1, [flatPair])
+            . Map.insert [U, C] (0, [UnpairedNuc, UnpairedNuc])
+            . Map.insert [C, G] (1, [flatPair])
+            . Map.insert [G, C] (1, [flatPair])
 
-            . Map.insert [A, U, G, G] 1
-            . Map.insert [U, G, G, C] 1
-            . Map.insert [G, G, C, A] 1
-            . Map.insert [G, C, A, U] 2
-            . Map.insert [C, A, U, C] 1
-            . Map.insert [A, U, C, G] 2
-            . Map.insert [U, C, G, G] 1
-            . Map.insert [C, G, G, C] 2
+            . Map.insert [A, U, G] (1, [flatPair, UnpairedNuc])
+            . Map.insert [U, G, G] (0, [UnpairedNuc, UnpairedNuc, UnpairedNuc])
+            . Map.insert [G, G, C] (1, [UnpairedNuc, flatPair])
+            . Map.insert [G, C, A] (1, [flatPair, UnpairedNuc])
+            . Map.insert [C, A, U] (1, [UnpairedNuc, flatPair])
+            . Map.insert [A, U, C] (1, [flatPair, UnpairedNuc])
+            . Map.insert [U, C, G] (1, [UnpairedNuc, flatPair])
+            . Map.insert [C, G, G] (1, [flatPair, UnpairedNuc])
+            . Map.insert [G, G, C] (1, [UnpairedNuc, flatPair])
 
-            . Map.insert [A, U, G, G, C] 2
-            . Map.insert [U, G, G, C, A] 1
-            . Map.insert [G, G, C, A, U] 2
-            . Map.insert [G, C, A, U, C] 2
-            . Map.insert [C, A, U, C, G] 2
-            . Map.insert [A, U, C, G, G] 2
-            . Map.insert [U, C, G, G, C] 2
+            . Map.insert [A, U, G, G] (1, [flatPair, UnpairedNuc, UnpairedNuc])
+            . Map.insert [U, G, G, C] (1, [UnpairedNuc, UnpairedNuc, flatPair])
+            . Map.insert [G, G, C, A] (1, [UnpairedNuc, flatPair, UnpairedNuc])
+            . Map.insert [G, C, A, U] (2, [flatPair, flatPair])
+            . Map.insert [C, A, U, C] (1, [UnpairedNuc, flatPair, UnpairedNuc])
+            . Map.insert [A, U, C, G] (2, [flatPair, flatPair])
+            . Map.insert [U, C, G, G] (1, [UnpairedNuc, flatPair, UnpairedNuc])
+            . Map.insert [C, G, G, C] (2, [flatPair, flatPair])
 
-            . Map.insert [A, U, G, G, C, A] 2
-            . Map.insert [U, G, G, C, A, U] 2
-            . Map.insert [G, G, C, A, U, C] 3
-            . Map.insert [G, C, A, U, C, G] 3
-            . Map.insert [C, A, U, C, G, G] 3
-            . Map.insert [A, U, C, G, G, C] 3
+            . Map.insert [A, U, G, G, C] (2, [flatPair, UnpairedNuc, flatPair])
+            . Map.insert [U, G, G, C, A] (2, [Pair [UnpairedNuc, flatPair]])
+            . Map.insert [G, G, C, A, U] (2, [UnpairedNuc, flatPair, flatPair])
+            . Map.insert [G, C, A, U, C] (2, [flatPair, flatPair, UnpairedNuc])
+            . Map.insert [C, A, U, C, G] (2, [UnpairedNuc, flatPair, flatPair])
+            . Map.insert [A, U, C, G, G] (2, [flatPair, flatPair, UnpairedNuc])
+            . Map.insert [U, C, G, G, C] (2, [UnpairedNuc, flatPair, flatPair])
 
-            . Map.insert [A, U, G, G, C, A, U] 3
-            . Map.insert [U, G, G, C, A, U, C] 3
-            . Map.insert [G, G, C, A, U, C, G] 3
-            . Map.insert [G, C, A, U, C, G, G] 3
-            . Map.insert [C, A, U, C, G, G, C] 3
 
-            . Map.insert [A, U, G, G, C, A, U, C] 4
-            . Map.insert [U, G, G, C, A, U, C, G] 3
-            . Map.insert [G, G, C, A, U, C, G, G] 3
-            . Map.insert [G, C, A, U, C, G, G, C] 4
+            . Map.insert [A, U, G, G, C, A]
+                (2, [flatPair, UnpairedNuc, flatPair, UnpairedNuc])
 
-            . Map.insert [A, U, G, G, C, A, U, C, G] 4
-            . Map.insert [U, G, G, C, A, U, C, G, G] 3
-            . Map.insert [G, G, C, A, U, C, G, G, C] 4
+            . Map.insert [U, G, G, C, A, U]
+                (2, [Pair [UnpairedNuc, flatPair], UnpairedNuc])
 
-            . Map.insert [A, U, G, G, C, A, U, C, G, G] 4
-            . Map.insert [U, G, G, C, A, U, C, G, G, C] 4
+            . Map.insert [G, G, C, A, U, C]
+                (3, [Pair [flatPair, flatPair]])
 
-            . Map.insert [A, U, G, G, C, A, U, C, G, G, C] 5
+            . Map.insert [G, C, A, U, C, G]
+                (3, [flatPair, flatPair, flatPair])
+
+            . Map.insert [C, A, U, C, G, G]
+                (3, [Pair [flatPair, flatPair]])
+
+            . Map.insert [A, U, C, G, G, C]
+                (3, [flatPair, flatPair, flatPair])
+
+
+            . Map.insert [A, U, G, G, C, A, U]
+                (3, [flatPair, UnpairedNuc, flatPair, flatPair])
+
+            . Map.insert [U, G, G, C, A, U, C]
+                (3, [UnpairedNuc, Pair [flatPair, flatPair]])
+
+            . Map.insert [G, G, C, A, U, C, G]
+                (3, [Pair [flatPair, flatPair], UnpairedNuc])
+
+            . Map.insert [G, C, A, U, C, G, G]
+                (3, [flatPair, flatPair, flatPair, UnpairedNuc])
+
+            . Map.insert [C, A, U, C, G, G, C]
+                (3, [Pair [flatPair, flatPair], UnpairedNuc])
+
+
+            . Map.insert [A, U, G, G, C, A, U, C]
+                (4, [flatPair, Pair [flatPair, flatPair]])
+
+            . Map.insert [U, G, G, C, A, U, C, G]
+                (3, [UnpairedNuc, Pair [flatPair, flatPair], UnpairedNuc])
+
+            . Map.insert [G, G, C, A, U, C, G, G]
+                (3, [Pair [flatPair, flatPair], UnpairedNuc, UnpairedNuc])
+
+            . Map.insert [G, C, A, U, C, G, G, C]
+                (4, [flatPair, flatPair, flatPair, flatPair])
+
+
+            . Map.insert [A, U, G, G, C, A, U, C, G]
+                (4, [Pair [], Pair [Pair [], Pair []], UnpairedNuc])
+
+            . Map.insert [U, G, G, C, A, U, C, G, G]
+                (3, [UnpairedNuc, Pair [flatPair, flatPair], UnpairedNuc, UnpairedNuc])
+
+            . Map.insert [G, G, C, A, U, C, G, G, C]
+                (4, [Pair [flatPair, flatPair], UnpairedNuc, flatPair])
+
+
+            . Map.insert [A, U, G, G, C, A, U, C, G, G]
+                (4, [flatPair, Pair [flatPair, flatPair], UnpairedNuc, UnpairedNuc])
+
+            . Map.insert [U, G, G, C, A, U, C, G, G, C]
+                (4, [UnpairedNuc, Pair [flatPair, flatPair], UnpairedNuc, flatPair])
+
+            . Map.insert [A, U, G, G, C, A, U, C, G, G, C]
+                (5, [flatPair, Pair [flatPair, flatPair], UnpairedNuc, flatPair])
 
             $ mempty
 
           diff s e = if s == e then Nothing else Just s
 
-      Map.differenceWith diff (scoreWatsonCrick rna) expectedMap `shouldBe` mempty
+      Map.differenceWith diff (scoreWatsonCrick rna) expectedMap
+        `shouldBe` mempty
